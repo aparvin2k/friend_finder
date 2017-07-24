84398e3a8be953d53c0250659564cc45e1e3c64d
aparvin2k <aparvin2005@gmail.com>
2017-07-24 15:49:42 -0700

    initial upload

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+  name: "Scott Summers",
+  photo: "https://i.annihil.us/u/prod/marvel//universe3zx/images/4/45/Cyclops_Head.jpg",
+  scores: [5, 3, 4, 2, 4, 4, 1, 2, 5, 1]
+}, {
+  name: "Robert Drake",
+  photo: "https://vignette4.wikia.nocookie.net/marveldatabase/images/3/30/Hulk_Vol_4_5_ResurrXion_Variant_Textless.jpg/revision/latest/scale-to-width-down/329?cb=20170406125800",
+  scores: [1, 5, 3, 1, 2, 4, 2, 4, 2, 1]
+}, {
+  name: "Remy Etienne LeBeau",
+  photo: "http://www.lediableblanc.com/Wallpaper/09.jpg",
+  scores: [2, 5, 5, 3, 5, 1, 5, 3, 2, 1]
+},{
+  name: "Hank McCoy",
+  photo: "https://s-media-cache-ak0.pinimg.com/736x/6e/d0/09/6ed009df5a2a2dc31e712f62a1b7464b--batman-vs-superman.jpg",
+  scores: [3, 2, 3, 5, 2, 4, 5, 1, 3, 1]
+}, {
+  name: "Jean Grey",
+  photo: "http://www.getjoys.net/sharingSub/1611/ej13124201056239943018520.png",
+  scores: [2, 3, 3, 3, 3, 1, 2, 4, 3, 1]
+}, {
+  name: "Anna Maria",
+  photo: "https://images4.alphacoders.com/147/147396.jpg",
+  scores: [3, 3, 2, 2, 2, 2, 3, 5, 3, 1]
+}, {
+  name: "Elizabeth Braddock",
+  photo: "http://4.bp.blogspot.com/-Mt3cOCpcR_g/TscKxt74D3I/AAAAAAAAAbI/vr8B3WJu18w/s1600/psylocke-background-4-769986.jpg",
+  scores: [2, 4, 3, 4, 2, 1, 3, 3, 3, 1]
+}];
+
+ //export array
+
+module.exports = friends;
